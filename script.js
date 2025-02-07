5404d791d178f3581b4592be88429f825835223f
Goober96 <volosia712@gmail.com>
2025-02-07 13:29:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple example for ripple effect or interactive fish animation (based on your preference)
+document.querySelector('.cta-button').addEventListener('mouseover', () => {
+    document.querySelector('.hero-image').classList.add('ripple-effect');
+});
